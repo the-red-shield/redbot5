6536e3999c8235a3798201ae1e4aed0daa5d0dab
Ian <img6662@rit.edu>
2025-02-05 18:52:26 -0500

    fixing jest test

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,17 @@
-// This file is intentionally left blank for now.
-// Add any Jest setup code here.
+// Set up any necessary global variables or configurations for Jest tests
+
+// Example: Mocking a global function
+global.fetch = require('node-fetch');
+
+// Example: Setting up environment variables
+process.env.NODE_ENV = 'test';
+
+// Example: Adding custom matchers from jest-extended
+import 'jest-extended';
+
+// Example: Mocking a module
+jest.mock('some-module', () => {
+  return {
+    someFunction: jest.fn().mockReturnValue('mocked value'),
+  };
+});
