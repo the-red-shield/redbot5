fc7eefc79633ea444cfd46b75332a73ff6c56914
Ian <img6662@rit.edu>
2025-02-06 00:56:50 -0500

    pollyfills for jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,16 @@
 export default {
-  setupFiles: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'node',
+  setupFiles: ['<rootDir>/jest.setup.js', '<rootDir>/jest.polyfills.js'], // Include jest.polyfills.js
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
     '^.+\\.tsx?$': 'babel-jest'
   },
-  testEnvironment: 'node',
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  moduleFileExtensions: ['js', 'jsx'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  testMatch: ['**/tests/**/*.test.js'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  moduleNameMapper: {
+    "^axios$": "axios/dist/node/axios.cjs" // Correct path as a string
+  }
 };
